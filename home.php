98b0e57d64fb1b989da7cca7b56029fa252462a5
LuizGustavoPereira <pereiraguus@gmail.com>
2018-04-08 14:42:08 -0300

    inicializando o projeto

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import materialize.css-->
+		<link rel="stylesheet" href="css/materialize.css">	   
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+<body>
+
+	<ul id="slide-out" class="sidenav sidenav-fixed">	 
+		<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+		<li><a href="#!">Second Link</a></li>
+		<li><div class="divider"></div></li>
+		<li><a class="subheader">Subheader</a></li>
+		<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+	</ul>
+	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="js/materialize.js"></script>
+	<script type="text/javascript" >
+
+	
+	</script>
+</body>
+</html>
