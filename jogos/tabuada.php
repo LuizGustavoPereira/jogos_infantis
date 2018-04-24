152de405a3dc50e1ab15a2626cb97dc03b717864
Gustavo Pereira <pereiraguus@gmail.com>
2018-04-23 23:51:17 -0300

    tabuada.php

--- jogos/tabuada.php
+++ jogos/tabuada.php
@@ -1,14 +1,14 @@
-
-<div class="row" display="none">
-        <div class="col l10 offset-l1 ">
-          <div class="card transparent darken-1">
-            <div class="card-content blue-text">
-            	<div class="center-align">
-            		<h3>Jogo da Tabuada</h3>
-            	</div>
-            	<div class="row">
-            		<div class="col l5 xl5 offset-l1 offset-xl1 ">
-            			<fieldset>
+<!-- Tela antes de começar o jogo -->
+<div class="row" id="inicio" display>
+    <div class="col l10 offset-l1 " >
+		<div class="card transparent darken-1">
+			<div class="card-content blue-text">
+				<div class="center-align">
+					<h3>Jogo da Tabuada</h3>
+				</div>
+				<div class="row">
+					<div class="col l5 xl5 offset-l1 offset-xl1 ">
+						<fieldset>
 						    <legend>Selecione a tabuada:</legend>
 						    <div class="row">
 						    	<div class="col l12 xl12 input-field">
@@ -23,9 +23,9 @@
 						    </div>
 
 					  </fieldset>
-            		</div>
-            		<div class="col l5 xl5">
-            			<fieldset>
+					</div>
+					<div class="col l5 xl5">
+						<fieldset>
 						    <legend>Selecione o tipo de entrada:</legend>
 
 						    <div class="row">
@@ -41,14 +41,37 @@
 						    </div>
 
 					  </fieldset>
-            		</div>
-            	</div>
-            	<div class="row center-align">
-            		<div class="col l10 offset-l1">
-            			<a class="waves-effect waves-light btn background btnControl" >COMEÇAR</a>
-            		</div>
-            	</div>
-            </div>            
-          </div>
-        </div>
-      </div>
+					</div>
+				</div>
+				<div class="row center-align">
+					<div class="col l10 offset-l1">
+						<a class="waves-effect waves-light btn background btnControl" onClick="comecarJogo()">COMEÇAR</a>
+					</div>
+				</div>
+			</div>            
+		</div>
+    </div>
+</div>
+
+<!-- Tela do jogo  -->
+<div class="row" id="jogo" display>
+    <div class="col l10 offset-l1 " >
+		<div class="card transparent darken-1">
+			<div class="card-content blue-text">
+				<div class="center-align">
+					<h3>Tabuada do </h3>
+				</div>				
+			</div>            
+		</div>
+    </div>
+</div>
+
+<script type="text/javascript">
+	function comecarJogo(){
+		$("#inicio").hide();
+		$("#jogo").show();
+	}
+	$(document).ready(function(){
+		$("#jogo").hide();
+	});
+</script>
