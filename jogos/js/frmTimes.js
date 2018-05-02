adb83493483b47ef1560f4a77144ef35762b65d7
LuizGustavoPereira <pereiraguus@gmail.com>
2018-05-02 10:57:10 -0300

    times

---
+++ jogos/js/frmTimes.js
@@ -0,0 +1,107 @@
+var resp = "",
+	sorteados = []
+	time = "";
+
+function comecarJogo(){
+	$("#jogo").show();
+	$("#telaInicial").hide();
+	$("#nomeTime").focus();
+	sorteiaTime();
+}
+
+function sorteiaTime(){
+	verificaTimes()	
+	switch (time){
+		case 1:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-botafogo.png'>");
+			resp = "BOTAFOGO";
+		break;
+		case 2:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-corinthians.png'>");
+			resp = "CORINTHIANS";
+		break;
+		case 3:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-flamengo.png'>");
+			resp = "FLAMENGO";
+		break;
+		case 4:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-fluminense.png'>");
+			resp = "FLUMINENSE";
+		break;
+		case 5:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-gremio.png'>");
+			resp = "GREMIO";
+		break;
+		case 6:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-internacional.png'>");
+			resp = "INTERNACIONAL";
+		break;
+		case 7:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-palmeiras.png'>");
+			resp = "PALMEIRAS";
+		break;
+		case 8:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-santos.png'>");
+			resp = "SANTOS";
+		break;
+		case 9:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-sao-paulo.png'>");
+			resp = "SÃO PAULO";
+		break;
+		case 10:
+			$("#logoTime").html("<img src='imagens/times/escudo-do-vasco.png'>");
+			resp = "VASCO";
+		break;
+	}
+		
+	
+}
+
+function verificaTimes(){
+	if(sorteados.length >= 10){
+		alert("PARABENS! VOCÊ CONSEGUIU");
+		$("#jogarNovamente").show();
+		$("#nomeTime").prop('disabled', true);
+
+	}else{		
+		time = Math.floor((Math.random() * 10) + 1);
+		if(sorteados.indexOf(time) > -1){
+			verificaTimes();
+		}else{	
+			sorteados.push(time);			
+		}
+	}
+}
+
+function verificaResposta(){
+	var resposta  = $("#nomeTime").val().toUpperCase();
+	$("#nomeTime").val("");
+	if( resposta == resp ){
+		alert("VOCÊ ACERTOU");
+		sorteiaTime();
+	}else{
+		alert("ERROU");
+	}
+
+}
+function jogarNovamente(){
+		sorteados = [];
+		$("#jogarNovamente").hide();
+		$("#nomeTime").prop('disabled', false);
+		comecarJogo();
+}
+function voltar(){
+	$("#jogo").hide();
+	$("#telaInicial").show();
+}
+
+$(document).ready(function(){
+	$("#jogo").hide();
+	$("#jogarNovamente").hide();
+
+	$("input[type='text']").bind('keydown',function(e) {
+		if(e.keyCode == '13'){
+			verificaResposta( );
+		}
+	});
+});
--- jogos/times.php
+++ jogos/times.php
@@ -1,4 +1,4 @@
-
+<!-- Pagína inicial -->
 <div class="row" >
 	<div class="col l10 offset-l1">
 		<div class="titulo ">
@@ -22,42 +22,35 @@
         </div>
     </div>
 </div>
-<div class="row" id="jogo" display>
-    <div class="col l10 offset-l1">
-        <div class="card transparent darken-1">
-            <div class="card-content blue-text text-darken-2"> 
-	            <div class="row">
-	            	<div class="center-align col l12" id="bandeira">
-	            		<img src='imagens/brasileiro/bandeira-preta-branca.png' class='img-brasileiro-bandeira'>
-            		</div>  
-            		<div class="center-align  col l2 offset-l1" >
-        				<img src='imagens/brasileiro/tinta-amarelo.png' class="img-brasileiro-tinta" onClick="pintaBandeira(2)">
-        			</div>
-        			<div class="center-align  col l2" >
-        				<img src='imagens/brasileiro/tinta-roxa.png' class="img-brasileiro-tinta" onClick="pintaBandeira(0)">
-        			</div>
-        			<div class="center-align col l2">
-        				<img src='imagens/brasileiro/tinta-azul.png' class="img-brasileiro-tinta" onClick="pintaBandeira(1)">	
-        			</div>
-        			<div class="center-align col l2">
-        				<img src='imagens/brasileiro/tinta-vermelha.png' class="img-brasileiro-tinta" onClick="pintaBandeira(0)">	
-        			</div>
-        			<div class="center-align col l2">
-        				<img src='imagens/brasileiro/tinta-verde.png' class="img-brasileiro-tinta" onClick="pintaBandeira(3)">   
-        			</div>
-            	</div>  
-            	<div class="row">
-            		<div class="col l10 offset-l1 center-align" id="nomeCor">
 
-            		</div>
-            	</div>          	
-	            <div class="row left-align">
+<!-- Pagína do jogo -->
+<div class="row">
+	<div class="col l10 offset-l1" id="jogo" display>
+        <div class="card transparent darken-1">
+            <div class="card-content "> 
+	            <div class="row">
+	            	<div class="col l6 offset-l4 center-align" >
+	            		<div class="card card-imagens small " style="max-height: 100% !important">
+					        <div class="card-ver-todos" style="max-height: 100% !important" id="logoTime">			        	
+								
+					        </div>							
+			     		</div>
+	            	</div>
+	            	<div class="col l5 offset-l3">
+	            		<input type="text" name="nomeTime" id="nomeTime" value="" class="input-times">
+	            	</div>
+            	</div> 
+            	<div class="row left-align">
 					<div class="col l4 ">
 						<a class="waves-effect waves-light btn background btnControl" onClick="voltar()"><i class="material-icons"> arrow_back </i></a>
-					</div>					
-				</div>	            
-            </div> 
+					</div>	
+					<div class="col l4">
+						<a class="waves-effect waves-light btn background btnControl" onClick="jogarNovamente()" id="jogarNovamente" display="">Jogar Novamente</a>
+					</div>				
+				</div>	           	
+            </div>            
         </div>
     </div>
 </div>
-<script src="jogos/js/frmBrasileiro.js" type="text/javascript"></script>
+
+<script src="jogos/js/frmTimes.js" type="text/javascript"></script>
