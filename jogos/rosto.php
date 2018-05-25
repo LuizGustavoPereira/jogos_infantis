fe95e99eab663e9677af635992911db50fca1f1c
bcampos4 <36507243+bcampos4@users.noreply.github.com>
2018-05-25 17:58:04 -0300

    add nariz palhaco

--- jogos/rosto.php
+++ jogos/rosto.php
@@ -8,14 +8,14 @@
 					
             	</div>  
             	<div class="row">
-            		<div class="col l10 offset-l1 center-align" id="nomeCor">
-            			<div style="background:  url(imagens/rosto/palhaco.jpg); width: 370px; height: 400px">					
-						<div id="DivDestino" ondrop="drop(event)" ondragover="allowDrop(event)"></div>						
-					</div>
-					<img ondrop="drop(event)" ondragover="allowDrop(event)" />
-						<img style="width: 7%" id="LogoDrag" src="imagens/rosto/emoticon.png" draggable="true" ondragstart="drag(event)"  />
-            		</div>
-            	</div>          	
+            		<div class="col l10 offset-l1 center-align" id="nomeCor" style="background:  url(imagens/rosto/palhaco.jpg); width: 370px; height: 400px">
+            				
+						<div id="DivDestino" ondrop="drop(event)" ondragover="allowDrop(event)"></div>					
+            		</div>	
+            	</div> 
+            	<div class="row" style="width: 10%">
+            		<img style="width: 100%"  id="LogoDrag" src="imagens/rosto/emoticon.png" draggable="true" ondragstart="drag(event)"  />
+            	</div>   	
 	            <div class="row left-align">
 					<div class="col l4 ">
 						<a class="waves-effect waves-light btn background btnControl" onClick="voltar()"><i class="material-icons"> arrow_back </i></a>
