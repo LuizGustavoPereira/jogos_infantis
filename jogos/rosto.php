d205013d3aef0ab2ffa6e979e482b246d148a29b
bcampos4 <36507243+bcampos4@users.noreply.github.com>
2018-05-29 19:14:31 -0300

    modificação rosto

--- jogos/rosto.php
+++ jogos/rosto.php
@@ -2,19 +2,18 @@
     <div class="col l10 offset-l1">
         <div class="card transparent darken-1">
             <div class="card-content blue-text text-darken-2"> 
-	            <div class="row"> 					
-					<br />
-					
-					
-            	</div>  
+	            
             	<div class="row">
-            		<div class="col l10 offset-l1 center-align" id="nomeCor" style="background:  url(imagens/rosto/palhaco.jpg); width: 370px; height: 400px">
-            				
-						<div id="DivDestino" ondrop="drop(event)" ondragover="allowDrop(event)"></div>					
+            		<div class="col l10 offset-l1 center-align" id="nomeCor">
+                        <div>
+                            <img style="width: 50%"  src="imagens/rosto/1.png">
+                            <div id="DivDestino" ondrop="drop(event)" ondragover="allowDrop(event)"></div>          
+                        </div>            			
+								
             		</div>	
             	</div> 
             	<div class="row" style="width: 10%">
-            		<img style="width: 100%"  id="LogoDrag" src="imagens/rosto/emoticon.png" draggable="true" ondragstart="drag(event)"  />
+            		<img style="width: 100%"  id="LogoDrag" src="imagens/rosto/cabelo.png" draggable="true" ondragstart="drag(event)"  />
             	</div>   	
 	            <div class="row left-align">
 					<div class="col l4 ">
