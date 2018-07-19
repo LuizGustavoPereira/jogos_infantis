6987bed210d22783187768ce2d7739cb96b91def
bcampos4 <36507243+bcampos4@users.noreply.github.com>
2018-07-18 22:26:44 -0300

    salada de frutas

--- jogos/js/frmSaladaFruta.js
+++ jogos/js/frmSaladaFruta.js
@@ -1,6 +1,6 @@
 var resp = "",
 	sorteados = [],
-	time = "",
+	fruta = "",
 	pontuacao = 0,
 	vidas = 3,
 	count = 0,
@@ -16,7 +16,6 @@ function comecarJogo(){
 	sorteados = [];	
 	clearTimeout(tempo);
 	count = 0;
-	tempo = setInterval("atualizaTempo()", 10000);
 	$("#min").html("<p>inicio</p>");
 	$("#pontuacao").html(pontuacao);
 	$("#jogo").show();
@@ -28,40 +27,35 @@ function comecarJogo(){
 }
 
 function sorteiaFruta(){
+	$("#divResp").html("");
+	$("#letrasFruta").html("");
+	input = 0;
+	index = 0;
 	nomeFruta = [];
 	posInput = 1;
-	verificaFrutas()	
-	switch (2){
+	verificaFrutas();	
+	switch (fruta){
 		case 1:
-			resp = "ABACAXI";
-			input = 0;
-			index = 0;
-			array_index = [1,2,3,4,5,6,7];
+			resp = "ABACAXI";			
+			array_index = [0,1,2,3,4,5,6];
 			array_letras=["A","C","A","B","A","I","X"];
-			$("#divResp").html("");
-			$("#letrasFruta").html("");
 			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/abacaxi.png'>");
 			for(var i=0; i < array_index.length; i++){
 				if((i%2 == 0)){
-					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-par' id=letra1>"+array_letras[i]+"</span>");
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-par' id=letra"+i+">"+array_letras[i]+"</span>");
 				}else{
-					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-impar' id=letra1>"+array_letras[i]+"</span>");
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-impar' id=letra"+i+">"+array_letras[i]+"</span>");
 				}
 			}
 			for(var i=0; i < array_index.length; i++){
-				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeTime"+(i+1)+"\" >");			
-			}
+				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
+			}	
 			
-			opcoes();
 		break;
 		case 2:
 			resp = "MELANCIA";
-			input = 0;
-			index = 0;
 			array_index = [0,1,2,3,4,5,6,7];
-			array_letras=["L","A","N","M","A","I","C","E"];
-			$("#divResp").html("");
-			$("#letrasFruta").html("");
+			array_letras=["L","A","N","M","A","I","C","E"];			
 			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/melancia.png'>");
 			for(var i=0; i < array_index.length; i++){
 				if((i%2 == 0)){
@@ -71,20 +65,17 @@ function sorteiaFruta(){
 				}
 			}
 			for(var i=0; i < array_index.length; i++){
-				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeTime"+(i+1)+"\" >");			
+				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
 			}
-			
-			opcoes();
 		break;
 	}	
+
+	opcoes();
 	$(document).keypress(function(e) {
     	if(e.which == 13) {
-    		var valor = $('#letra'+array_index[index-1]).text();
-    		alert(valor);
-    		
-    		if(valor == resp[input]){
-    			$('#letra'+array_index[index-1]).hide();
-    			console.log(resp[input])    			
+    		var valor = $('#letra'+array_index[index-1]).text();  
+    		alert(valor);  		
+    		if(valor == resp[input]){  			
     			$("#nomeFruta"+input).val(valor)
     			array_index.splice(index-1, 1);
     			input+=1
@@ -112,18 +103,18 @@ function opcoes(){
 }
 
 function verificaFrutas(){
-	if(sorteados.length >= 1){
+	if(sorteados.length >= 2){
 		alert("PARABENS! VOCÊ CONSEGUIU");
-		clearTimeout(tempo);
-		$("#jogarNovamente").show();
-		$("#nomeTime").prop('disabled', true);
+		
 
 	}else{		
-		time = Math.floor((Math.random() * 10) + 1);
-		if(sorteados.indexOf(time) > -1){
-			verificaTimes();
+		fruta = Math.floor((Math.random() * 2) + 1);
+		if(sorteados.indexOf(fruta) > -1){
+			alert();
+			verificaFrutas();
+
 		}else{	
-			sorteados.push(time);			
+			sorteados.push(fruta);			
 		}
 	}
 }
@@ -149,31 +140,7 @@ function voltar(){
 	$("#telaInicial").show();
 }
 
-function atualizaTempo(){
-	if(count < 6 ){
-		color = "green";
-	}else if(count < 12){
-		color = "blue";
-	}else if (count < 17){
-		color = "red";
-	}
-	else{
-		$("#nomeTime").prop('disabled', true);
-		$("#jogarNovamente").show();
-		clearTimeout(tempo);
-	}
-	if(count == 6){
-		$("#min").append("&nbsp;&nbsp;&nbsp;<p>1 min</p>");
-	}
-	else if(count == 12){
-		$("#min").append("&nbsp;&nbsp;&nbsp;<p>2 min</p>");
-	}
-	else if(count == 17){
-		$("#min").append("&nbsp;&nbsp;&nbsp;<p>3 min</p>");
-	}
-	$("#countTempo").append("<div style='background-color:"+color+"'></div>");
-	count ++;	
-}
+
 
 $(document).ready(function(){
 	$("#jogo").hide();
--- jogos/saladaFruta.php
+++ jogos/saladaFruta.php
@@ -29,9 +29,9 @@
         <div class="card transparent darken-1">
             <div class="card-content "> 
 	            <div class="row">
-	            	<div class="col l7  xl8 offset-xl0" >
+	            	<div class="col l7 xl8 offset-xl0 margin-top-5" >
 	            		
-		            	<div class="col xl4 card card-imagens small float-left">
+		            	<div class="col xl4 card card-imagens float-left">
 						    <div id="imagemFruta">			        	
 									
 						    </div>							
@@ -39,7 +39,7 @@
 			     		<div class="col xl7 offset-xl1 letras-salada-fruta" id="letrasFruta">
 			     			
 			     		</div>
-			     		<div class="col l12 center-align" id="divResp">
+			     		<div class="col l12 center-align margin-top-5" id="divResp">
 	            			
 	            		</div>
 	            	</div>
@@ -51,19 +51,6 @@
 						<fieldset>
 							<legend>Vidas</legend>
 							<h5><b class="red-text" id="vidas"></b><i class="material-icons red-text">favorite</i></h5>
-						</fieldset>
-						<fieldset>
-							<legend>Tempo</legend>
-							<fieldset class="tempo">
-								<div class="row">
-									<div class="col l7" id="min">
-										
-									</div>									
-									<div class="col l5" id="countTempo">
-										&nbsp;
-									</div>
-								</div>
-							</fieldset>
 						</fieldset>
 					</div>
 	            	
