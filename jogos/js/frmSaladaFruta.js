996335cb45cc4fed7a641b3d9127ec31e95ddba5
bcampos4 <36507243+bcampos4@users.noreply.github.com>
2018-07-18 22:36:25 -0300

    imagem abacaxi

--- jogos/js/frmSaladaFruta.js
+++ jogos/js/frmSaladaFruta.js
@@ -34,11 +34,11 @@ function sorteiaFruta(){
 	nomeFruta = [];
 	posInput = 1;
 	verificaFrutas();	
-	switch (fruta){
+	switch (1){
 		case 1:
-			resp = "ABACAXI";			
+			resp = "ABACAXI";
 			array_index = [0,1,2,3,4,5,6];
-			array_letras=["A","C","A","B","A","I","X"];
+			array_letras=["B","A","C","X","A","I","A"];			
 			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/abacaxi.png'>");
 			for(var i=0; i < array_index.length; i++){
 				if((i%2 == 0)){
@@ -68,6 +68,22 @@ function sorteiaFruta(){
 				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
 			}
 		break;
+		case 3:
+			resp = "BANANA";
+			array_index = [0,1,2,3,4,5];
+			array_letras=["N","A","A","B","A","N"];			
+			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/banana.png'>");
+			for(var i=0; i < array_index.length; i++){
+				if((i%2 == 0)){
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-par' id=letra"+i+">"+array_letras[i]+"</span>");
+				}else{
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-impar' id=letra"+i+">"+array_letras[i]+"</span>");
+				}
+			}
+			for(var i=0; i < array_index.length; i++){
+				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
+			}
+		break;
 	}	
 
 	opcoes();
@@ -86,6 +102,7 @@ function sorteiaFruta(){
 }
 
 function opcoes(){
+	console.log("oi");
 	if((array_index).length > 0){
 		if(array_index[index] == null){
 			$("#letra"+(array_index[index-1])).removeClass("animacao-fruta");
@@ -103,12 +120,12 @@ function opcoes(){
 }
 
 function verificaFrutas(){
-	if(sorteados.length >= 2){
+	if(sorteados.length >= 3){
 		alert("PARABENS! VOCÊ CONSEGUIU");
 		
 
 	}else{		
-		fruta = Math.floor((Math.random() * 2) + 1);
+		fruta = Math.floor((Math.random() * 3) + 1);
 		if(sorteados.indexOf(fruta) > -1){
 			alert();
 			verificaFrutas();
