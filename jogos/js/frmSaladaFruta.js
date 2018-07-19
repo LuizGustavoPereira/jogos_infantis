65bf6c48c5dd9f15a1c3a9cd2dc23ecfc6150483
bcampos4 <36507243+bcampos4@users.noreply.github.com>
2018-07-18 22:41:53 -0300

    inclusõa de frutas salada de frutas

--- jogos/js/frmSaladaFruta.js
+++ jogos/js/frmSaladaFruta.js
@@ -34,7 +34,7 @@ function sorteiaFruta(){
 	nomeFruta = [];
 	posInput = 1;
 	verificaFrutas();	
-	switch (1){
+	switch (6){
 		case 1:
 			resp = "ABACAXI";
 			array_index = [0,1,2,3,4,5,6];
@@ -84,18 +84,68 @@ function sorteiaFruta(){
 				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
 			}
 		break;
+		case 4:
+			resp = "LARANJA";
+			array_index = [0,1,2,3,4,5,6];
+			array_letras=["L","A","J","A","A","N","R"];			
+			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/laranja.png'>");
+			for(var i=0; i < array_index.length; i++){
+				if((i%2 == 0)){
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-par' id=letra"+i+">"+array_letras[i]+"</span>");
+				}else{
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-impar' id=letra"+i+">"+array_letras[i]+"</span>");
+				}
+			}
+			for(var i=0; i < array_index.length; i++){
+				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
+			}
+		break;
+		case 5:
+			resp = "LIMÃO";
+			array_index = [0,1,2,3,4];
+			array_letras=["L","M","O","Ã","I"];			
+			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/limao.png'>");
+			for(var i=0; i < array_index.length; i++){
+				if((i%2 == 0)){
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-par' id=letra"+i+">"+array_letras[i]+"</span>");
+				}else{
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-impar' id=letra"+i+">"+array_letras[i]+"</span>");
+				}
+			}
+			for(var i=0; i < array_index.length; i++){
+				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
+			}
+		break;
+		case 6:
+			resp = "MAÇÃ";
+			array_index = [0,1,2,3];
+			array_letras=["Ç","M","A","Ã"];			
+			$("#imagemFruta").html("<img class='width-100' src='imagens/salada-de-frutas/maca.png'>");
+			for(var i=0; i < array_index.length; i++){
+				if((i%2 == 0)){
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-par' id=letra"+i+">"+array_letras[i]+"</span>");
+				}else{
+					$("#letrasFruta").append("<span class='float-left letra-salada-fruta-impar' id=letra"+i+">"+array_letras[i]+"</span>");
+				}
+			}
+			for(var i=0; i < array_index.length; i++){
+				$("#divResp").append("<input type=\"text\" name=\"nome\" id=\"nomeFruta"+i+"\" value=\"\" class=\"input-times\" maxlength=\"1\" data-next=\"#nomeFruta"+(i+1)+"\" >");			
+			}
+		break;
 	}	
 
 	opcoes();
 	$(document).keypress(function(e) {
     	if(e.which == 13) {
     		var valor = $('#letra'+array_index[index-1]).text();  
-    		alert(valor);  		
     		if(valor == resp[input]){  			
     			$("#nomeFruta"+input).val(valor)
     			array_index.splice(index-1, 1);
     			input+=1
     			index-=1
+    		}else{
+    			vidas-=1;
+    			$("#vidas").html(vidas);
     		}
     	}
 	});
