2bbeac8d880ef7ba25c5a80d9185a975c8985ea8
Gustavo Pereira <pereiraguus@gmail.com>
2019-11-05 01:07:02 -0300

    05/11/2019
    organização da página inicial dos jogos

--- jogos/paginaInicial.php
+++ jogos/paginaInicial.php
@@ -5,13 +5,93 @@
 			<h3>VER TODOS</h3>
 		</div>
 		<div class="row">
+			<div class="col l3 ">
+		    	<a href="./jogos/alfabeto">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/alfabeto/alfabeto.png">
+				        </div>		        
+						<span class="titulo card-title blue-text">Alfabeto</span>
+			     	</div>
+			    </a>
+	    	</div>
+	    	<div class="col l3 ">
+		    	<a href="./jogos/animais">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/animais/capa.png">
+				        </div>		        
+						<span class="titulo card-title blue-text">Animais</span>
+			     	</div>
+			    </a>
+	    	</div>	    	
+	    	<div class="col l3 ">
+		    	<a href="./jogos/antes-depois">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/antes-depois/antes-depois.png">
+				        </div>		        
+						<span class="titulo card-title deep-purple-text">Antes e Depois</span>
+			     	</div>
+			    </a>
+	    	</div>
 		    <div class="col l3">
 		    	<a href="./jogos/brasileiro">
 			      	<div class="card card-imagens small ">
 				        <div class="card-image card-ver-todos" >			        	
 							<img src="imagens/brasileiro/ze-carioca.png">
 				        </div>
-						<span class="card-title green-text">Brasileiro</span>				        	       
+						<span class="titulo card-title green-text">Brasileiro</span>				        	       
+			     	</div>
+			    </a>
+	    	</div>	    		    
+	    	<div class="col l3 ">
+		    	<a href="./jogos/monteiro-lobato">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/monteiro-lobato/monteiro-lobato.png">
+				        </div>		        
+						<span class="titulo card-title amber-text">Brincando com Monteiro Lobato</span>
+			     	</div>
+			    </a>
+	    	</div>
+	    	<div class="col l3 ">
+		    	<a href="./jogos/casa-s">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/descubra-palavra/detetive.gif">
+				        </div>		        
+						<span class="titulo card-title amber-text">Casa do S</span>
+			     	</div>
+			    </a>
+	    	</div>
+	    	<div class="col l3 ">
+		    	<a href="./jogos/descubra-palavra">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/descubra-palavra/detetive.gif">
+				        </div>		        
+						<span class="titulo card-title amber-text">Descubra a Palavra</span>
+			     	</div>
+			    </a>
+	    	</div>
+	    	<div class="col l3 ">
+		    	<a href="./jogos/matematica">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/matematica/matematica.png">
+				        </div>		        
+						<span class="titulo card-title indigo-text">Matemática</span>
+			     	</div>
+			    </a>
+	    	</div>
+	    	<div class="col l3 ">
+		    	<a href="./jogos/charadas">
+			      	<div class="card card-imagens small ">
+				        <div class="card-image card-ver-todos" >			        	
+							<img src="imagens/charada/duvidas.png">
+				        </div>		        
+						<span class="titulo card-title amber-text">O que é o que é?</span>
 			     	</div>
 			    </a>
 	    	</div>
@@ -21,7 +101,7 @@
 				        <div class="card-image card-ver-todos" >			        	
 							<img src="imagens/tabuada/tabuada.png">
 				        </div>		        
-						<span class="card-title blue-text">Tabuada</span>
+						<span class="titulo card-title blue-text">Tabuada</span>
 			     	</div>
 			    </a>
 	    	</div>
@@ -31,40 +111,21 @@
 				        <div class="card-image card-ver-todos" >			        	
 							<img src="imagens/times/futebol.png">
 				        </div>		        
-						<span class="card-title amber-text">Times</span>
+						<span class="titulo card-title amber-text">Times</span>
 			     	</div>
 			    </a>
 	    	</div>
 	    	<div class="col l3 ">
-		    	<a href="./jogos/alfabeto">
+		    	<a href="./jogos/salada-fruta">
 			      	<div class="card card-imagens small ">
 				        <div class="card-image card-ver-todos" >			        	
-							<img src="imagens/alfabeto/alfabeto.png">
+							<img src="imagens/salada-de-frutas/salada-fruta.png">
 				        </div>		        
-						<span class="card-title amber-text">Alfabeto</span>
+						<span class="titulo card-title amber-text">Salada de frutas</span>
 			     	</div>
 			    </a>
 	    	</div>
-	    	<div class="col l3 ">
-		    	<a href="./jogos/antes-depois">
-			      	<div class="card card-imagens small ">
-				        <div class="card-image card-ver-todos" >			        	
-							<img src="imagens/antes-depois/antes-depois.png">
-				        </div>		        
-						<span class="card-title amber-text">Antes e Depois</span>
-			     	</div>
-			    </a>
-	    	</div>
-	    	<div class="col l3 ">
-		    	<a href="./jogos/casa-s">
-			      	<div class="card card-imagens small ">
-				        <div class="card-image card-ver-todos" >			        	
-							<img src="imagens/antes-depois/antes-depois.png">
-				        </div>		        
-						<span class="card-title amber-text">Casa do S</span>
-			     	</div>
-			    </a>
-	    	</div>
+	    	
   		</div>
     </div>
 </div>
